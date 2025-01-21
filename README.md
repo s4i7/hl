d21f5388ce9ec758c967f9c55db209b81125dffc
s4i7 <s4i7@pm.me>
2025-01-21 21:59:14 +0000

    x

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hl
 wip
+
+1) run on host in case of hsa out of resources error
+```
+sudo sysctl -w vm.max_map_count=262144
+```
--- hl/gemm.hpp
+++ hl/gemm.hpp
@@ -1,1 +1,4 @@
 #pragma once
+#include <hip/hip_runtime.h>
+
+__global__ void matmulKernel(const float* A, const float* B, float* C, int M, int N, int K);
